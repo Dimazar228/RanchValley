b1a2ac1648268427fb1731f5b2e0a8df11427b7e
Dimazar228 <Dimazar228@yandex.ru>
2023-01-04 18:22:28 +0300

    Importing of Player

---
+++ code/level.py
@@ -0,0 +1,22 @@
+import pygame
+from settings import *
+from player import Player
+
+
+class Level:
+    def __init__(self):
+        # get the display
+        self.display_surface = pygame.display.get_surface()
+
+        # all sprites
+        self.all_sprites = pygame.sprite.Group()
+
+        self.setup()
+
+    def setup(self):
+        self.player = Player((640, 360), self.all_sprites)  # DON'T FORGET TO CHANGE THIS VALUE
+
+    def run(self, dt):
+        self.display_surface.fill('black')
+        self.all_sprites.draw(self.display_surface)
+        self.all_sprites.update(dt)
